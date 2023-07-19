bdfaffdf4d2e4e4b951bca0e81aade11c6b954e7
David Rodriguez <Daviderrr10@gmail.com>
2023-07-18 20:40:45 -0400

    js questions

---
+++ assets/script.js
@@ -0,0 +1,80 @@
+var questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyperlinks and Text Markup Language",
+      "HyperWeb and Text Markup Language",
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question: "Inside which HTML element do we put the Javascript?",
+    answer: "script",
+    options: ["js", "scripting", "script", "javascript"],
+  },
+  {
+    numb: 4,
+    question: "Which property is used to change the background color in CSS?",
+    answer: "background-color",
+    options: ["bg", "color-background", "bgcolor", "background-color"],
+  },
+  {
+    numb: 5,
+    question: "Javascript is the same as java?",
+    answer: "False",
+    options: ["True", "False", "45", "Neither"],
+  },
+
+  {
+    numb: 6,
+    question: "Which property is used to change the font of an element?",
+    answer: "font-style",
+    options: ["font-family", "font-weight", "font-change", "font-style"],
+  },
+  {
+    numb: 7,
+    question: "Commonly used data types DO NOT include?",
+    answer: "alerts",
+    options: ["strings", "alerts", "booleans", "numbers"],
+  },
+  {
+    numb: 8,
+    question: "Arrays in JavaScript can be used to store?",
+    answer: "all of the above",
+    options: [
+      "booleans",
+      "other arrays",
+      "numbers and strings",
+      "all of the above",
+    ],
+  },
+  {
+    numb: 9,
+    question:
+      "String values must be enclosed within __ when being assigned to variables?",
+    answer: "parenthesis",
+    options: ["commas", "curly brackets", "qoutes", "parenthesis"],
+  },
+  {
+    numb: 10,
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is?",
+    answer: "console.log",
+    options: ["terminal or bash", "console.log", "for loops", "Javascript"],
+  },
+];
